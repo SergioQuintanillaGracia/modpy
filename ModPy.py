608b9d222c704ac9a2eb4f73b830da7ceba9e3c6
SergioQuintanillaGracia <sergioquintanillagracia@gmail.com>
2022-06-03 22:16:13 +0200

    Update ModPy.py

--- ModPy.py
+++ ModPy.py
@@ -3,6 +3,10 @@ from tkinter import *
 root = Tk()
 root.title("ModPy Alpha 0.1")
 root.geometry("350x500")  #Resolution of the window
-root.resizable = False
+root.resizable(False, False)  #Make it so you can't resize it
+
+
+
+
 
 root.mainloop()
