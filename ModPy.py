bdd83d2f0fc0ef7cac7e3b7726bbca3d1fbcd4f0
SergioQuintanillaGracia <sergioquintanillagracia@gmail.com>
2022-06-03 20:46:38 +0200

    Sketch updates

--- ModPy.py
+++ ModPy.py
@@ -2,7 +2,7 @@ from tkinter import *
 
 root = Tk()
 root.title("ModPy Alpha 0.1")
-root.geometry("800x500")  #Resolution of the window
+root.geometry("350x500")  #Resolution of the window
 root.resizable = False
 
 root.mainloop()
