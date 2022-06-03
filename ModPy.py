e08ca731e590945273c8226c8964eadc4bdcff7d
SergioQuintanillaGracia <sergioquintanillagracia@gmail.com>
2022-06-03 23:41:29 +0200

    Button panel frame
    Added button panel frame and its buttons. I have to fix the png background of the settings cog.

--- ModPy.py
+++ ModPy.py
@@ -18,10 +18,10 @@ def create_button_panel():
     button_panel_frame = Frame(root, width = 350 - widget_offset_x * 2, height = 60, highlightbackground="#afafaf", highlightthickness=1)
     button_panel_frame.place(in_ = root, x = widget_offset_x, y = widget_offset_y)
 
-    plus_sign_image = ImageTk.PhotoImage(Image.open("images/plus_sign.png"))
     add_button = Button(
+        text = "+",
         font = ("Arial", 55),
-        image = plus_sign_image, compound = "c",
+        image = virtualPixel, compound = "c",
         width = 60 - widget_offset_x * 4 + 2,
         height = 60 - widget_offset_x * 4 + 2,
         command = print)
@@ -34,7 +34,33 @@ def create_button_panel():
         width = 60 - widget_offset_x * 4 + 2,
         height = 60 - widget_offset_x * 4 + 2,
         command = print)
-    remove_button.place(in_ = button_panel_frame, x = 60 + widget_offset_x, y = 60 + widget_offset_y)
+    remove_button.place(in_ = button_panel_frame, x = 58, y = widget_offset_y)
+
+    import_from_file_button = Button(
+        text = "Import from file",
+        font = ("Arial", 14),
+        image = virtualPixel, compound = "c",
+        width = 180 - widget_offset_x * 4 + 2,
+        height = 60 - widget_offset_x * 4 + 2,
+        command = print)
+    import_from_file_button.place(in_ = button_panel_frame, x = 110, y = widget_offset_y)
+
+    import_from_file_button = Button(
+        text = "Import from file",
+        font = ("Arial", 14),
+        image = virtualPixel, compound = "c",
+        width = 180 - widget_offset_x * 4 + 2,
+        height = 60 - widget_offset_x * 4 + 2,
+        command = print)
+    import_from_file_button.place(in_ = button_panel_frame, x = 110, y = widget_offset_y)
+
+    global settings_cog  #If you don't make an image variable global, you won't be able to use it
+    settings_cog = ImageTk.PhotoImage(Image.open("images/settings_cog.png"))
+    settings_button = Button(
+        relief = "flat",
+        image = settings_cog,
+        command = print)
+    settings_button.place(in_ = button_panel_frame, x = 283, y = widget_offset_y - 2)
 
 
 create_button_panel()
