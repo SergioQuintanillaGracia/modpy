7c8d88e70beaa69987e3a9256649711169a73924
SergioQuintanillaGracia <sergioquintanillagracia@gmail.com>
2022-06-03 22:45:11 +0200

    Adding images

--- ModPy.py
+++ ModPy.py
@@ -1,16 +1,43 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 root = Tk()
 root.title("ModPy Alpha 0.1")
 root.geometry("350x500")  #Resolution of the window
 root.resizable(False, False)  #Make it so you can't resize it
 
-frame_offset_x = 6
-frame_offset_y = frame_offset_x
+#Frame offset variables are used to separate the frames from the edges of the window
+widget_offset_x = 6
+widget_offset_y = widget_offset_x
+
+#virtualPixel is used to make the buttons completely resizable
+virtualPixel = ImageTk.PhotoImage(Image.open("images/virtualPixel.png"))
 
 
-button_frame = Frame(root, width = 350 - frame_offset_x * 2, height = 60, highlightbackground="#afafaf", highlightthickness=1)
-button_frame.place(x = frame_offset_x, y = frame_offset_y)
+def create_button_panel():
+    button_panel_frame = Frame(root, width = 350 - widget_offset_x * 2, height = 60, highlightbackground="#afafaf", highlightthickness=1)
+    button_panel_frame.place(in_ = root, x = widget_offset_x, y = widget_offset_y)
+
+    add_button = Button(
+        text = "+",
+        font = ("Arial", 55),
+        image = virtualPixel, compound = "c",
+        width = 60 - widget_offset_x * 4 + 2,
+        height = 60 - widget_offset_x * 4 + 2,
+        command = print)
+    add_button.place(in_ = button_panel_frame, x = widget_offset_x, y = widget_offset_y)
+
+    remove_button = Button(
+        text = "-",
+        font = ("Arial", 55),
+        image = virtualPixel, compound = "c",
+        width = 60 - widget_offset_x * 4 + 2,
+        height = 60 - widget_offset_x * 4 + 2,
+        command = print)
+    remove_button.place(in_ = button_panel_frame, x = 60 + widget_offset_x, y = 60 + widget_offset_y)
+
+
+create_button_panel()
 
 
 root.mainloop()
